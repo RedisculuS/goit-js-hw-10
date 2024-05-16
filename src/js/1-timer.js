cf9a0779222d1467b794cc6a87dc1b81e812146f
RedisculuS <Altunina311@gmail.com>
2024-05-16 18:50:25 +0300

    3rd commit
    Tasks done. Before mentor`s check

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,9 +1,12 @@
 'use strict';
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 let userSelectedDate;
 let countdownInterval;
+document.querySelector('[data-start]').disabled = true;
 
 const options = {
   enableTime: true,
@@ -11,11 +14,16 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
     userSelectedDate = selectedDates[0];
     const currentDate = new Date();
     if (userSelectedDate < currentDate) {
-      window.alert('Please choose a date in the future');
+      iziToast.show({
+        title: 'Please',
+        message: ' choose a date in the future',
+        backgroundColor: '#FFA000',
+        messageColor: '#FFFFFF',
+        titleColor: '#FFFFFF',
+      });
       document.querySelector('[data-start]').disabled = true;
     } else {
       document.querySelector('[data-start]').disabled = false;
@@ -57,58 +65,18 @@ document.querySelector('[data-start]').addEventListener('click', function () {
   document.querySelector('[data-start]').disabled = true;
   countdownUpdate();
   countdownInterval = setInterval(countdownUpdate, 1000);
-
-  function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-  }
-  function addLeadingZero(value) {
-    return value < 10 ? `0${value}` : value;
-  }
-
-  let timerInterval;
-
-  document.querySelector('[data-start]').addEventListener('click', function () {
-    const selectedDate = new Date(
-      document.getElementById('datetime-picker').value
-    ).getTime();
-    const currentDate = new Date().getTime();
-    const distance = selectedDate - currentDate;
-
-    if (distance < 0) {
-      alert('Please choose a date in the future');
-      return;
-    }
-
-    clearInterval(timerInterval);
-
-    timerInterval = setInterval(function () {
-      const { days, hours, minutes, seconds } = convertMs(distance);
-
-      document.querySelector('[data-days]').innerText = addLeadingZero(days);
-      document.querySelector('[data-hours]').innerText = addLeadingZero(hours);
-      document.querySelector('[data-minutes]').innerText =
-        addLeadingZero(minutes);
-      document.querySelector('[data-seconds]').innerText =
-        addLeadingZero(seconds);
-
-      distance -= 1000;
-
-      if (distance < 0) {
-        clearInterval(timerInterval);
-      }
-    }, 1000);
-
-    this.disabled = true;
-    document.getElementById('datetime-picker').disabled = true;
-  });
 });
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
